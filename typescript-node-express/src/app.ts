0e2aa887e3320625974d97712aa17c582d1cbadf
kangjietan <kangjietan@gmail.com>
2020-12-23 01:37:01 -0800

    TypeScript Node Express Starter

---
+++ typescript-node-express/src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
