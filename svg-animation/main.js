9cf2f854ffdb108e1240266c3c9f16c4f0db07fa
kangjietan <kangjietan@gmail.com>
2021-03-03 20:30:53 -0800

    Add function to handle css

--- svg-animation/main.js
+++ svg-animation/main.js
@@ -1,5 +1,19 @@
-const kang = document.querySelectorAll("#kang path");
-console.log(kang);
-kang.forEach((node) => {
-  console.log(node.getTotalLength());
-});
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    function animateSVG(id, delay = 0, delayIncrement) {
+      const logo = document.getElementById(id);
+      const logoPaths = document.querySelectorAll(`#${id} path`);
+      logo.style.animation = `fill 0.5s ease forwards ${delayIncrement * (logoPaths.length + 2.5)}s`;
+      for (let i = 0; i < logoPaths.length; i++) {
+        logoPaths[i].style.strokeDasharray = logoPaths[i].getTotalLength();
+        logoPaths[i].style.strokeDashoffset = logoPaths[i].getTotalLength();
+        logoPaths[i].style.animation = `line-anim 2s ease forwards ${delay}s`;
+        delay += delayIncrement;
+      }
+    }
+    // Set the id of SVG, delay time in seconds to start animation and delay between each animation
+    animateSVG("kang", 0, 0.4);
+  },
+  false
+);
